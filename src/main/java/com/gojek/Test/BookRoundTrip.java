a2deb9d2fc99abb5b67ee2c786ded5665edfe473
chandraprakashvimal <chandra90prakash@gmail.com>
2016-12-19 22:23:06 +0530

    Added test

--- src/main/java/com/gojek/Test/BookRoundTrip.java
+++ src/main/java/com/gojek/Test/BookRoundTrip.java
@@ -1,5 +1,25 @@
 package com.gojek.Test;
 
-public class BookRoundTrip {
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import com.gojek.Base.BaseScript;
+import com.gojek.Pages.Home;
+
+public class BookRoundTrip extends BaseScript {
+
+	@Test
+	public void RoundTrip() throws Exception {
+		try {
+
+			Home home = new Home(driver);
+
+			WebDriverWait wait = new WebDriverWait(driver, 60);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		}
+	}
 
 }
