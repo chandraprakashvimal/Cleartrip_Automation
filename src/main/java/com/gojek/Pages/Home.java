097591588c4dbde98ed7202301887fc8557978a5
chandraprakashvimal <chandra90prakash@gmail.com>
2016-12-19 22:37:49 +0530

    fill the booking form

--- src/main/java/com/gojek/Pages/Home.java
+++ src/main/java/com/gojek/Pages/Home.java
@@ -1,6 +1,9 @@
 package com.gojek.Pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Home {
@@ -10,6 +13,48 @@ public class Home {
 		// TODO Auto-generated constructor stub
 		this.driver = wd;
 		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//*[@id='SearchBtn']")
+	public WebElement searchFlightBtn;
+
+	@FindBy(xpath = "//*[@id='Home']/div/aside[1]/nav/ul[1]/li[1]")
+	public WebElement flightsTab;
+
+	@FindBy(xpath = "//*[@id='RoundTrip']")
+	public WebElement roundTripBtn;
+
+	@FindBy(xpath = "//*[@id='FromTag']")
+	public WebElement sourceTextBox;
+
+	@FindBy(xpath = "//*[@id='ToTag']")
+	public WebElement destTextBox;
+
+	@FindBy(xpath = "//*[@id='ui-datepicker-div']")
+	public WebElement datepicker;
+
+	@FindBy(xpath = "//*[@id='ResultContainer_1_1']//*[@class='booking fRight']")
+	public WebElement bookFlightBtn;
+	
+	public Boolean selectSource(String source) {
+		sourceTextBox.clear();
+		sourceTextBox.sendKeys(source);
+		sourceTextBox.sendKeys(Keys.TAB);
+
+		if (sourceTextBox.getText().equals(source))
+			return true;
+		else
+			return false;
+	}
+
+	public Boolean selectDetination(String destination) {
+		destTextBox.clear();
+		destTextBox.sendKeys(destination);
+		destTextBox.sendKeys(Keys.TAB);
+		if (destTextBox.getText().equals(destination))
+			return true;
+		else
+			return false;
 	}
 
 }
--- src/main/java/com/gojek/Test/BookRoundTrip.java
+++ src/main/java/com/gojek/Test/BookRoundTrip.java
@@ -1,6 +1,8 @@
 package com.gojek.Test;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.gojek.Base.BaseScript;
@@ -15,6 +17,23 @@ public class BookRoundTrip extends BaseScript {
 			Home home = new Home(driver);
 
 			WebDriverWait wait = new WebDriverWait(driver, 60);
+			wait.until(ExpectedConditions.elementToBeClickable(home.searchFlightBtn));
+
+			Assert.assertEquals(driver.getTitle(), "Cleartrip - Flights, Hotels, Local, Trains, Packages");
+
+			// click on flights tab is not selected
+			if (!home.flightsTab.isSelected())
+				home.flightsTab.click();
+
+			// click on round trip button if not selected
+			if (!home.roundTripBtn.isSelected()) {
+				home.roundTripBtn.click();
+			}
+
+			// Enter source and destination
+			home.selectSource("New Delhi, IN - Indira Gandhi Airport (DEL)");
+			home.selectDetination("Lucknow, IN - Amausi (LKO)");
+			Thread.sleep(1000);
 
 		} catch (Exception e) {
 			// TODO: handle exception
